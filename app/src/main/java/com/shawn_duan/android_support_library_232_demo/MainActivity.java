9c773f982146c6d03a3b7b7d4466f8ce42c1306f
Shawn Duan <duan1216@gmail.com>
2016-03-11 01:02:17 -0800

    Initial commit, with a demo for AnimatedVectorDrawable

---
+++ app/src/main/java/com/shawn_duan/android_support_library_232_demo/MainActivity.java
@@ -0,0 +1,60 @@
+package com.shawn_duan.android_support_library_232_demo;
+
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mAnimVectorDrawableImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mAnimVectorDrawableImageView = (ImageView) findViewById(R.id.anim_vector_drawable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Drawable drawable = mAnimVectorDrawableImageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable)drawable).start();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
